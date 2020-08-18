571d9b60a8d32b9f612e21d53d15b662c75cfadc
Pat Kirby <pkirby@dssinc.com>
2020-08-18 11:07:16 -0700

    08 18 20 @1106

--- GradeBook/GradeBooks/RandkedGradeBook.cs
+++ GradeBook/GradeBooks/RandkedGradeBook.cs
@@ -16,5 +16,52 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationsException("You must have 5 students to do ranked gradig.");
+            }
+                
+            var threshold = (int) Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            
+            if (grades[threshold -1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrades)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrades)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrades)
+                return 'D';
+            else
+                return 'F';
+            
+        }
+            
+
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires 5 students");
+                return;
+
+            }
+            base.CalculateStatistics();
+        }
+       
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires 5 students");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
